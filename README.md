a5b48d14b5e9047043373cce1f3b32102e390e2b
claudio <claudio@Android-AP.local>
2020-04-11 16:49:11 -0300

    Creates the readme

--- README.md
+++ README.md
@@ -1,1 +1,143 @@
-# Easy Media In Js
+# `Easy Media In Js`
+[![npm version](https://badge.fury.io/js/easy-media-in-js.svg)](https://badge.fury.io/js/easy-media-in-js)
+![core size](https://img.shields.io/bundlephobia/min/easy-media-in-js?label=core%20size)
+![core gzip size](https://img.shields.io/bundlephobia/minzip/easy-media-in-js.svg?label=core%20gzip%20size)
+
+## Demo 
+[👀 Demo Sandbox](https://codesandbox.io/s/easy-css-in-js-cgidd)
+
+## Descrition
+> Everybody knows that works with media query could be a mess, there have a lot possibilites of breakpoints, thinking in this and after study the material design of google i created this library to help to handle with most common breakpoint sizes. This library works with CSS-in-JS such as Emotion and Styled Components.
+
+This imagem show the possibilties of breakpoints.
+
+<img srcset="https://miro.medium.com/max/552/1*AyDtnhKNPvTsHMESCSqprw.png 276w, https://miro.medium.com/max/1104/1*AyDtnhKNPvTsHMESCSqprw.png 552w, https://miro.medium.com/max/1280/1*AyDtnhKNPvTsHMESCSqprw.png 640w, https://miro.medium.com/max/1456/1*AyDtnhKNPvTsHMESCSqprw.png 728w, https://miro.medium.com/max/1632/1*AyDtnhKNPvTsHMESCSqprw.png 816w, https://miro.medium.com/max/1808/1*AyDtnhKNPvTsHMESCSqprw.png 904w, https://miro.medium.com/max/1984/1*AyDtnhKNPvTsHMESCSqprw.png 992w, https://miro.medium.com/max/2000/1*AyDtnhKNPvTsHMESCSqprw.png 1000w" sizes="1000px" role="presentation" src="https://miro.medium.com/max/1920/1*AyDtnhKNPvTsHMESCSqprw.png">
+
+Well, it's imposible use all of it. Based in this guide i chosen the most importants breakpoints:
+
+```js
+const sizes = {
+  smallPhone: 300, // To Handle with phones like Iphone 5S
+  phone: 600,
+  tablet: 960,
+  desktop: 1280, 
+  largeDesktop: 1600 
+}
+```
+
+You can override the rules or create your own, just use `setSizes()`.
+
+```jsx
+
+import useMedia, { setSizes } from 'easy-media-in-js'
+// Use With Emotion
+import styled from '@emotion/styled';
+// Use With Styled Components
+import styled from 'styled-components';
+
+setSites({
+  smallPhone: 300, // To Handle with phones like Iphone 5S
+  phone: 600,
+  tablet: 960,
+  desktop: 1280, 
+  largeDesktop: 1600,
+  customBreakpoint: 1800
+})
+
+// and use it: 
+
+const Element = styled.div`
+  ${useMedia('customBreapoint')} {
+    [css property] : [property value];
+  }
+`
+```
+
+## Install 
+
+Yarn
+```bash
+  yarn add easy-media-in-js
+```
+NPM
+```bash
+  npm install easy-media-in-js --save-dev
+```
+
+## Usage
+
+Import the modules inside your file: 
+
+```jsx
+import useMedia from 'easy-media-in-js'
+// Use With Emotion
+import styled from '@emotion/styled';
+// Use With Styled Components
+import styled from 'styled-components';
+
+const Element = styled.div`
+  ${useMedia('phone')} {
+    border: 1px solid red;
+  }
+`
+```
+
+Output 
+```css
+  @media (min-width: 600px) {
+    border: 1px solid red;
+  }
+```
+
+### Possibilities
+
+```jsx
+import useMedia from 'easy-media-in-js'
+// Use With Emotion
+import styled from '@emotion/styled';
+// Use With Styled Components
+import styled from 'styled-components';
+
+const Element = styled.div`
+  
+  ${useMedia('<= phone')} {
+    border: 1px solid red;
+  }  
+
+  ${useMedia('phone')} {
+    border: 1px solid red;
+  }
+
+  ${useMedia('phone  < tablet')} {
+    border: 1px solid red;
+  }
+  ${useMedia('phone  < tablet < desktop < largeDesktop')} {
+    border: 1px solid red;
+  }
+  
+`
+```
+Outputs: 
+
+```css
+  
+  @media (max-width: 599px) {
+    border: 1px solid red;
+  }
+
+  @media (min-width: 600px) {
+    border: 1px solid red;
+  }
+
+  @media (min-width: 600px) and (min-width: 960px) {
+  border: 1px solid red;
+}
+
+@media (min-width: 600px) and (min-width: 960px) and (min-width: 1600px) {
+  border: 1px solid red;
+}
+
+```
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
 let sizes = {
-  small: 300,
-  phone: 480,
-  tablet: 720,
+  smallPhone: 300,
+  phone: 600,
+  tablet: 960,
   desktop: 1280,
-  largeDesktop: 1440
-}
+  largeDesktop: 1600,
+};
 
-const sizesNames = Object.keys(sizes).join('|');
+const sizesNames = Object.keys(sizes).join("|");
 
-const rgxMediaRules = new RegExp(`(^(${sizesNames}) ([<=|>=|>|< ${sizesNames} <=|>=|>|<]+) (${sizesNames})$|(^(<=|>=|>|<) (${sizesNames})$)|(^(${sizesNames})$))`, 'g')
+const rgxMediaRules = new RegExp(
+  `(^(${sizesNames}) ([<=|>=|>|< ${sizesNames} <=|>=|>|<]+) (${sizesNames})$|(^(<=|>=|>|<) (${sizesNames})$)|(^(${sizesNames})$))`,
+  "g"
+);
 
-const rgxConditions = /<=|>=|>|</g
+const rgxConditions = /<=|>=|>|</g;
 
-const validateParam = media => {
+const validateParam = (media) => {
   if (typeof media !== "string")
     throw new Error(
       `Invalid parameter, provide a string instead ${typeof media}`
-    )
+    );
 
   if (!media.trim().length)
-    throw new Error("Your string is empty, please provide a valid query")
+    throw new Error("Your string is empty, please provide a valid query");
   // TODO point to documentation
   if (!rgxMediaRules.test(media.trim()))
     throw new Error(
       "Your media query rule seems not match with a valid pattern, please provide a valid query!"
-    )
+    );
 
   media
     .trim()
@@ -33,70 +36,71 @@ const validateParam = media => {
       if (accumulator === "" && accumulator === currentValue)
         throw new Error(
           "Your string has extra spaces between words, please fix it."
-        )
-      return currentValue
-    })
-}
+        );
+      return currentValue;
+    });
+};
 
-const unSerializeString = medias => {
-  const mediaRules = medias.trim().match(rgxMediaRules)[0]
-  let previosWasEquals = { value: false }
+const unSerializeString = (medias) => {
+  const mediaRules = medias.trim().match(rgxMediaRules)[0];
+  let previosWasEquals = { value: false };
 
   return mediaRules
     .split(" ")
     .map(mapMediaQuery.bind(null, previosWasEquals))
-    .reduce(serializeMediaQuery, "@media")
-}
+    .reduce(serializeMediaQuery, "@media");
+};
 
 const mapMediaQuery = (previosWasEquals, rule, _index, array) => {
   const isCondition = rgxConditions.test(rule);
-  if( !isCondition && typeof sizes[rule] != 'number' ) throw new Error(`Your rule ${rule} with value ${sizes[rule]} must be a Number.`);
+  if (!isCondition && typeof sizes[rule] != "number")
+    throw new Error(
+      `Your rule ${rule} with value ${sizes[rule]} must be a Number.`
+    );
 
   if (array.length === 1) return `(min-width: ${sizes[rule]}px)`;
-  
 
   if (isCondition) {
-    previosWasEquals.value = /=/g.test(rule)
-    return />/g.test(rule) ? "(min-width: " : "(max-width: "
+    previosWasEquals.value = /=/g.test(rule);
+    return />/g.test(rule) ? "(min-width: " : "(max-width: ";
   }
-
 
   const lastRule = previosWasEquals.value
     ? `${sizes[rule] - 1}px)`
-    : `${sizes[rule]}px)`
+    : `${sizes[rule]}px)`;
 
-  previosWasEquals.value = false
+  previosWasEquals.value = false;
 
-  return lastRule
-}
+  return lastRule;
+};
 
 const serializeMediaQuery = (accumulator, currentValue, index, array) => {
   console.log(accumulator, currentValue);
-  if (array.length <= 2) return accumulator + currentValue
-  
-  const isFirstValue = index == 0 && /px/g.test(currentValue)
-  const isCondition = /-/g.test(currentValue)
-  const arrayLength = array.length - 1
+  if (array.length <= 2) return accumulator + currentValue;
 
-  const firsResult = `(min-width: ${currentValue} and `
+  const isFirstValue = index == 0 && /px/g.test(currentValue);
+  const isCondition = /-/g.test(currentValue);
+  const arrayLength = array.length - 1;
+
+  const firsResult = `(min-width: ${currentValue} and `;
   const secondResult =
     index !== arrayLength && !isCondition
       ? `${currentValue} and `
-      : currentValue
+      : currentValue;
 
-  return accumulator + (isFirstValue ? firsResult : secondResult)
-}
+  return accumulator + (isFirstValue ? firsResult : secondResult);
+};
 
-const setSizes = newSizes => {
-  sizes = newSizes
-}
+const setSizes = (newSizes) => {
+  sizes = newSizes;
+};
 
-const useMedia = media => {
-  validateParam(media)
-  const mediaQuery = unSerializeString(media)
+const useMedia = (media) => {
+  validateParam(media);
+  const mediaQuery = unSerializeString(media);
 
-  return mediaQuery
-}
+  return mediaQuery;
+};
 
-export default useMedia
-export { setSizes }
+export default useMedia;
+export { setSizes };
